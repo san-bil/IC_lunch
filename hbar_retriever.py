54d039ed65da58dab57ae9edc12554d5a7a06f9d
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-03-16 11:13:04 +0000

    hbar getter

---
+++ hbar_retriever.py
@@ -0,0 +1,20 @@
+import pycurl
+from StringIO import StringIO
+from bs4 import BeautifulSoup
+
+buffer = StringIO()
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://www3.imperial.ac.uk/eatinganddrinking/cateringoutlets/hbar/cafemenu')
+c.setopt(c.WRITEDATA, buffer)
+c.perform()
+c.close()
+
+body = buffer.getvalue()
+soup = BeautifulSoup(body)
+
+main_items=soup.find("table",{"id":"rg588883"}).find("div",{"id":"main-content"}).find_all("strong")
+
+for item in main_items:
+    txt = item.get_text()
+    if (not ("allergy" in txt or "Allergy" in txt or "served" in txt)) and len(txt)>6:
+        print item.get_text()
