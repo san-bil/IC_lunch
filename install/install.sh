ff2604b319bad8733250efda805e46c718c1bd27
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-03-17 11:48:11 +0000

    fixed some installer bugs

--- install/install.sh
+++ install/install.sh
@@ -4,25 +4,23 @@ echo  "Creating $HOME/bin/IC_lunch folder..."
 mkdir -p $HOME/bin/IC_lunch
 cp -R $( dirname $( dirname $( readlink -f $0 ) ) )/* $HOME/bin/IC_lunch/
 
-if [ -f $HOME/bin/IC_lunch/requirements.txt  ]
-then
-    echo -e "Installing requirements...\n"
-    pip install --user -r $HOME/bin/IC_lunch/requirements.txt
-fi
 
-mv $( dirname $( readlink -f $0 ) )/.add_path_example.txt $HOME/.path_adder
+echo -e "Installing requirements...\n"
+pip install --user beautifulsoup4
+
+mv $( dirname $( readlink -f $0 ) )/path_add $HOME/.path_add
 
 if [ -f $HOME/.bashrc ]
 then
-    echo 'source $HOME/.path_adder' >> $HOME/.bashrc
+    echo 'source $HOME/.path_add' >> $HOME/.bashrc
 fi
 
 if [ -f $HOME/.zshrc ]
 then
-    echo 'source $HOME/.path_adder' >> $HOME/.zshrc
+    echo 'source $HOME/.path_add' >> $HOME/.zshrc
 fi
 
 if [ -f $HOME/.cshrc  ]
 then
-    echo 'source $HOME/.path_adder' >> $HOME/.cshrc
+    echo 'source $HOME/.path_add' >> $HOME/.cshrc
 fi
